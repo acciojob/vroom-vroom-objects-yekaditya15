a4731ac42abe0de387e8e0e6fbff9c4389196c37
Yekaditya Devadi <141273781+yekaditya15@users.noreply.github.com>
2024-12-09 12:34:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-// Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+    this.make = make;
+    this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = function() {
+    return `${this.make} ${this.model}`;
+};
+
+function SportsCar(make, model, topSpeed) {
+    Car.call(this, make, model); // Inherit properties from Car
+    this.topSpeed = topSpeed;
+}
+
+// Inherit Car prototype
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
+
+
 
 // Do not change the code below
 window.Car = Car;
